5d15d1f07bd14650f59f19cfcd9b82dde39f219e
cfngc4594 <cfngc4594@gmail.com>
2025-02-17 15:16:37 +0800

    feat: integrate LanguageToggle component into Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Editor } from '@monaco-editor/react'
 import { shikiToMonaco } from '@shikijs/monaco'
 import { SUPPORTED_THEMES } from '@/constants/theme'
 import { ModeToggle } from '@/components/mode-toggle'
+import LanguageToggle from '@/components/language-toggle'
 import { SUPPORTED_LANGUAGES } from '@/constants/language'
 import { useCodeEditorStore } from '@/store/useCodeEditorStore'
 
@@ -10,7 +11,11 @@ export default function Home() {
   const { language, theme, setMonaco, setEditor } = useCodeEditorStore()
 
   return (
-    <div className='relative'>
+    <div className='h-screen flex flex-col'>
+      <header className='h-16 flex items-center justify-between p-4 gap-x-2 border-b'>
+        <LanguageToggle />
+        <ModeToggle />
+      </header>
       <Editor
         language={language}
         theme={theme}
@@ -19,20 +24,14 @@ export default function Home() {
             themes: [
               ...SUPPORTED_THEMES
             ],
-            langs: [
-              ...SUPPORTED_LANGUAGES
-            ]
+            langs: SUPPORTED_LANGUAGES.map(lang => lang.id)
           })
           shikiToMonaco(highlighter, monaco)
           setMonaco(monaco)
         }}
         onMount={(editor) => setEditor(editor)}
-        options={{ minimap: { enabled: false } }}
-        className='min-h-screen'
+        options={{ minimap: { enabled: false }, automaticLayout: true, padding: { top: 8 } }}
       />
-      <div className='fixed top-5 right-10'>
-        <ModeToggle />
-      </div>
     </div>
   )
 }
